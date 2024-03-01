8c5686213b79afbda8d26290124cf0acdc3f0a8b
Vlad Ispas <vlad.ispas@yahoo.com>
2024-03-01 13:28:27 +0200

    Created Xcode project.

---
+++ Fitness/Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Vlad Ispas on 01.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
