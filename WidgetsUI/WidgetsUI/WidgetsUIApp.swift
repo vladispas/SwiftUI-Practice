029f0bab7e58c4cb075e2da1a056f344e6efbced
Vlad Ispas <vlad.ispas@yahoo.com>
2024-03-07 16:38:36 +0200

    Created Xcode project.

---
+++ WidgetsUI/WidgetsUI/WidgetsUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetsUIApp.swift
+//  WidgetsUI
+//
+//  Created by Vlad Ispas on 07.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct WidgetsUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
