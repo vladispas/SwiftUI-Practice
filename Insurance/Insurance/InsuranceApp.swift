47b3a1d2de2e86e70bc433da0de35976968a592f
Vlad Ispas <vlad.ispas@yahoo.com>
2024-03-14 22:13:37 +0200

    Created Xcode project.

---
+++ Insurance/Insurance/InsuranceApp.swift
@@ -0,0 +1,17 @@
+//
+//  InsuranceApp.swift
+//  Insurance
+//
+//  Created by Vlad Ispas on 14.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct InsuranceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
